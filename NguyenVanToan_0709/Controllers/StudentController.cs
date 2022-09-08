24181b534ede11218ff79d67b3827f598a793ca8
nguyenvantoan604 <toanvan1112001@gmail.com>
2022-09-08 13:13:31 +0700

    Commit_Note

---
+++ NguyenVanToan_0709/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NguyenVanToan_0709.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
